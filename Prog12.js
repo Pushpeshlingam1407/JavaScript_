1da6bfd5f30e0299a80916db984835d7deaff2dc
Pushpesh Lingam <pushpeshlingam@gmail.com>
2026-06-22 22:36:03 +0530

    Format code for consistency and readability across multiple files

--- Prog12.js
+++ Prog12.js
@@ -1,6 +1,6 @@
 //TODO: ARRAYS
-let nums =[1,2,3,4,5,6,7,8,9,10];
-console.log(nums[0]); //1 
+let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(nums[0]); //1
 console.log(nums.length); //10
 console.log(nums);
 nums.push(8);
@@ -9,12 +9,11 @@ console.log(nums);
 nums.pop();
 console.log(nums);
 
-nums.shift();   
+nums.shift();
 console.log(nums);
 
 nums.unshift(1);
 console.log(nums);
 
-nums=nums.splice(2,3);
+nums = nums.splice(2, 3);
 console.log(nums);
-
