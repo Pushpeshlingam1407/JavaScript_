bc648cf10bc858e4faaf63eaff7944ef91600a8e
Pushpesh Lingam <pushpeshlingam@gmail.com>
2026-06-23 13:32:56 +0530

    feat: add demonstration script for JavaScript array manipulation methods

--- JS_Fundamentals/Prog12.js
+++ JS_Fundamentals/Prog12.js
@@ -1,20 +1,20 @@
 //TODO: ARRAYS
 let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-console.log(nums[0]); // 1
-console.log(nums.length); // 10
-console.log(nums);
+console.log("first element: ", nums[0]); // 1
+console.log("length of array: ", nums.length); // 10
+console.log(" array: ", nums);
 nums.push(8); // adds an element to the end of the array
-console.log(nums);
+console.log("after push: ", nums);
 
 nums.pop(); // removes the last element from the array
-console.log(nums);
+console.log("after pop: ", nums);
 
 nums.shift(); // removes the first element from the array
-console.log(nums);
+console.log("after shift: ", nums);
 
 nums.unshift(1); // add an element to the start of the array
-console.log(nums);
+console.log("after unshift: ", nums);
 
 nums = nums.splice(2, 3); // (startIndex, deleteCount) remove elements from array
-console.log(nums);
+console.log("after splice: ", nums);
 
