c8c81184b03823dd2f73179f51fb857ee627c214
Pushpesh Lingam <pushpeshlingam@gmail.com>
2026-06-23 19:13:01 +0530

    feat: add array manipulation scripts for concat, spread, splice, slice, and destructing methods

--- JS_Fundamentals/Prog14.js
+++ JS_Fundamentals/Prog14.js
@@ -2,12 +2,12 @@ let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 console.log(nums);
 let nums1 = [1, 3, 6, 67, 64, 56, 89, 100, 189, 134, 167, 145, 156, 178, 190];
 let total = nums.concat(nums1);
-console.log(total);
+console.log("Using Concat(): " + total);
 
 //TODO: Spread Operator
 //Combine two arrays using spread operator
 let total1 = [...nums, ...nums1];
-console.log(total1);
+console.log("Using Spread Operator: " + total1);
 
 let values = [41, 24, 55];
 // let [c,b,a]=values;
@@ -16,5 +16,5 @@ let values = [41, 24, 55];
 // console.log(c);
 
 let [a, , b] = values;
-console.log(a);
-console.log(b);
+console.log("First Element: " + a);
+console.log("Third Element: " + b);
--- JS_Fundamentals/Prog15.js
+++ JS_Fundamentals/Prog15.js
@@ -1,20 +1,25 @@
 //TODO: SPLICE
-//syntax array.splice(start_index,no_of_element,adding_elements)
+//* syntax: array.splice(start_index,no_of_element_to_delete,adding_elements)
 let nums = [1, 2, 3, 4, 5, 6, 7, 8];
-// console.log(nums);
-// nums.splice(2,0,100,200);
-// console.log(nums);
+console.log(nums);
+nums.splice(2, 0, 100, 200);
+console.log("After Splice: ");
+console.log(nums);
 
 //TODO: SLICE
 //syntax array.slice(startindex,endindex)
 let oorum = nums.slice(2, 5);
-console.log(oorum);
+console.log("Sliced Array: ");
+console.log(oorum); 
 
+console.log("OG Array: ");
 console.log(nums);
 delete nums[2];
+console.log("After Delete: ");
 console.log(nums);
 
 nums = "Hello my name is jon snow".split(" ");
+console.log("String Split: ");
 console.log(nums);
 if (Array.isArray(nums)) {
   console.log("true");
@@ -22,5 +27,6 @@ if (Array.isArray(nums)) {
 
 //TODO: INCLUDES
 let p = ["Jon", "snow", "is", "a", "bastard"];
+console.log("Includes: ");
 console.log(p.includes("snow"));
 console.log(p.includes("hello"));
