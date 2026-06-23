9d1555698b403928e462a1c84c4933b15a5552f8
Pushpesh Lingam <pushpeshlingam@gmail.com>
2026-06-23 23:44:07 +0530

    feat: add examples for Array.find and Array.every methods

--- JS_Fundamentals/Prog19.js
+++ JS_Fundamentals/Prog19.js
@@ -1,14 +1,14 @@
-//TODO: Find, every functions
+//TODO: Find, Every functions
 
 //TODO: Find
 //* syntax: array.find((element)=>{return element>value})
-//* it returns the first element in the array that satisfies the provided testing function. Otherwise undefined is returned.
+//* It returns the first element in the array that satisfies the provided testing function. Otherwise undefined is returned.
 const arr = [1, 2, 3, 4, 5, 6];
 let res = arr.find((x) => x > 3);
 console.log(res);
 
-function cv(el) {
-  return el > 4;
+function cv(ele) {
+  return ele > 4;
 }
 res = arr.find(cv);
 console.log(res);
@@ -23,3 +23,5 @@ function ce(ele) {
 }
 res = arr.every(ce);
 console.log(res);
+
+console.log(arr.every((x) => x > 0));
