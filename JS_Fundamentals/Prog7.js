8e37d28f1adfa2adb8946e849f16fb3598dc990b
Pushpesh Lingam <pushpeshlingam@gmail.com>
2026-06-23 10:50:15 +0530

    feat: add Prog7.js to demonstrate various JavaScript assignment operators

--- JS_Fundamentals/Prog7.js
+++ JS_Fundamentals/Prog7.js
@@ -1,8 +1,8 @@
 //assignment operators
 let a = 2,
   b = 8;
-console.log((a **= b));
-console.log((a += b));
-console.log((a -= b));
-console.log((a /= b));
-console.log((a %= b));
+console.log("a to the power of b:" + (a **= b));
+console.log("a plus b:" + (a += b));
+console.log("a minus b:" + (a -= b));
+console.log("a divided by b:" + (a /= b));
+console.log("a modulo b:" + (a %= b));
