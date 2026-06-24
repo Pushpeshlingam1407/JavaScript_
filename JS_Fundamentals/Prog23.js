b738c9713b8c2cb0427872cc47443b23a293e9bd
Pushpesh Lingam <pushpeshlingam@gmail.com>
2026-06-24 13:32:25 +0530

    feat: add examples for anonymous functions, IIFEs, and closures

--- JS_Fundamentals/Prog23.js
+++ JS_Fundamentals/Prog23.js
@@ -10,18 +10,32 @@ setTimeout(function () {
 console.log("3");
 
 //TODO: Call stack and Event loop
-//1. Call stack is a data structure that is used to store the function calls.
-//2. Event loop is a mechanism that is used to handle the asynchronous code in javascript.
-//3. When a function is called, it is added to the call stack. When the function is executed, it is removed from the call stack.
-//4. When an asynchronous function is called, it is added to the event queue. The event loop checks the call stack and if it is empty, it takes the first function from the event queue and adds it to the call stack.
+//*1. Call stack is a data structure that is used to store the function calls.
+//*2. Event loop is a mechanism that is used to handle the asynchronous code in javascript.
+//*3. When a function is called, it is added to the call stack. When the function is executed, it is removed from the call stack.
+//*4. When an asynchronous function is called, it is added to the event queue. The event loop checks the call stack and if it is empty, it takes the first function from the event queue and adds it to the call stack.
 
-//What are the advantages of using anonymous functions?
-//1. Anonymous functions are easier to write and read.
-//2. Anonymous functions can be used as arguments to other functions.
-//3. Anonymous functions can be used to create closures.
-//4. Anonymous functions can be used to create IIFEs (Immediately Invoked Function Expressions).
+//TODO: What are the advantages of using anonymous functions?
+//*1. Anonymous functions are easier to write and read.
+//*2. Anonymous functions can be used as arguments to other functions.
+//*3. Anonymous functions can be used to create closures.
+//*4. Anonymous functions can be used to create IIFEs (Immediately Invoked Function Expressions).
 
 //TODO: IIFE (Immediately Invoked Function Expression)
 (function (a, b) {
   console.log("This is an IIFE", a, b);
 })(20, 30);
+
+//TODO: When and Why IIFE is needed?
+//* IIFE is used to create a private scope for the variables.
+//* It is also used to avoid the pollution of the global scope.
+//* It is also used to create closures.
+//* It is also used to create modules.
+
+(function () {
+  const a = 10;
+  console.log("Inside the IIFE", a);
+})();
+
+//* console.log("Outside the IIFE", a);
+
---
+++ JS_Fundamentals/Prog34.js
@@ -0,0 +1,28 @@
+//TODO: Closures
+//* A closure is a function that has access to its own scope,
+//* the outer function's scope, and the global scope.
+
+function outer() {
+  const a = 10;
+  function inner() {
+    console.log(a);
+  }
+  return inner;
+}
+
+const res = outer();
+res();
+
+//TODO: Why closure is needed?
+//* Closures are used to create private variables.
+//* Closures are also used to create higher-order functions.
+//* Closures are also used to create modules.
+
+function makeAdder(x) {
+  return function (y) {
+    return x + y;
+  };
+}
+
+const add5 = makeAdder(5);
+console.log(add5(2)); // 7
