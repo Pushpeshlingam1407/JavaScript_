eeccc762d679c11a31042a3fffe27dbd97393e4e
Pushpesh Lingam <pushpeshlingam@gmail.com>
2026-06-26 11:32:23 +0530

    feat: add constructor function implementation example for object creation

--- JS_Fundamentals/Prog26.js
+++ JS_Fundamentals/Prog26.js
@@ -1,26 +1,27 @@
 //TODO: Construction functions
-//1. A constructor function is a function that is used to create objects.
+//* 1. A constructor function is a function that is used to create objects.
 
 function Person(name = null, age = null) {
   this.name = name;
   this.age = age;
 }
 
-//Like deep copy
+// Like deep copy
 const p1 = new Person("Pushpesh", 21);
 const p2 = new Person("Jon snow", 25);
 const p3 = new Person();
-console.log(p1.name);
-console.log(p1.age);
+console.log("Name: " + p1.name);
+console.log("Age: " + p1.age);
 
 p1.age = 60;
-console.log(p1.age);
-console.log(p2.name);
-console.log(p2.age);
-
-console.log(p3.name);
-console.log(p3.age);
+console.log("Age: " + p1.age);
+console.log("Name: " + p2.name);
+console.log("Age: " + p2.age);
+console.log("\n================\n");
+console.log("Name: " + p3.name);
+console.log("Age: " + p3.age);
 p3.name = "Arya Stark";
 p3.age = 18;
-console.log(p3.name);
-console.log(p3.age);
+console.log("\n================\n");
+console.log("Name: " + p3.name);
+console.log("Age: " + p3.age);
