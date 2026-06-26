e8275c0310add35645bb82cafa17fe8dc0269010
Pushpesh Lingam <pushpeshlingam@gmail.com>
2026-06-26 11:33:38 +0530

    feat: add example program demonstrating constructor functions in JavaScript

--- JS_Fundamentals/Prog26.js
+++ JS_Fundamentals/Prog26.js
@@ -12,16 +12,21 @@ const p2 = new Person("Jon snow", 25);
 const p3 = new Person();
 console.log("Name: " + p1.name);
 console.log("Age: " + p1.age);
+console.log("\n================\n");
 
 p1.age = 60;
 console.log("Age: " + p1.age);
+console.log("\n================\n");
+
 console.log("Name: " + p2.name);
 console.log("Age: " + p2.age);
 console.log("\n================\n");
+
 console.log("Name: " + p3.name);
 console.log("Age: " + p3.age);
+console.log("\n================\n");
 p3.name = "Arya Stark";
 p3.age = 18;
-console.log("\n================\n");
+
 console.log("Name: " + p3.name);
 console.log("Age: " + p3.age);
