f2dc3a6f038462e403d0a3330c19611ff74641d4
Pushpesh Lingam <pushpeshlingam@gmail.com>
2026-06-26 11:11:51 +0530

    feat: add implementation and documentation for higher-order functions example

--- JS_Fundamentals/Prog24.js
+++ JS_Fundamentals/Prog24.js
@@ -1,6 +1,6 @@
 //TODO: Higher order functions
-//1. A higher order function is a function that takes one or more functions as arguments and/or returns a function as its result.
-//2. Higher order functions are used to create abstractions and to make code more reusable.
+//* 1. A higher order function is a function that takes one or more functions as arguments and/or returns a function as its result.
+//* 2. Higher order functions are used to create abstractions and to make code more reusable.
 
 function sum(a, b) {
   return a + b;
